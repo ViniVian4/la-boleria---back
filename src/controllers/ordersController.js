3a39685e33bedd88eb1e0cb83340c3193ad15d43
Vinicius <vianaxd99@gmail.com>
2022-10-24 01:38:05 -0300

    get /orders/:id

--- src/controllers/ordersController.js
+++ src/controllers/ordersController.js
@@ -80,4 +80,59 @@ async function getOrders(req, res) {
     }
 }
 
-export { postOrders, getOrders }
+async function getOrderId(req, res) {
+    const { id } = req.params;
+
+    try {
+        const order = await connection.query(`
+        SELECT 
+            orders.id AS "orderId",
+            orders."createdAt",
+            orders.quantity,
+            orders."totalPrice",
+            clients.id AS "clientId",
+            clients.name AS "clientName",
+            clients.address,
+            clients.phone,
+            cakes.id AS "cakeId",
+            cakes.name AS "cakeName",
+            cakes.price,
+            cakes.description,
+            cakes.image FROM orders
+        JOIN clients
+            ON orders."clientId" = clients.id
+        JOIN cakes
+            ON orders."cakeId" = cakes.id WHERE orders.id=$1;`, [id]);
+
+        if (order.rows.length === 0) {
+            return res.sendStatus(404);
+        }
+
+        const finalOrder = 
+        {
+            client: {
+                id: order.rows[0].clientId,
+                name: order.rows[0].clientName,
+                address: order.rows[0].address,
+                phone: order.rows[0].phone
+            },
+            cake: {
+                id: order.rows[0].cakeId,
+                name: order.rows[0].cakeName,
+                price: order.rows[0].price,
+                description: order.rows[0].description,
+                image: order.rows[0].image
+            },
+            orderId: order.rows[0].orderId,
+            createdAt: `${order.rows[0].createdAt.getFullYear()}-${order.rows[0].createdAt.getMonth()}-${order.rows[0].createdAt.getDate()} ${order.rows[0].createdAt.getHours()}:${order.rows[0].createdAt.getMinutes()}`,
+            quantity: order.rows[0].quantity,
+            totalPrice: order.rows[0].totalPrice
+        }
+
+        res.status(200).send(finalOrder);
+    } catch (error) {
+        return res.sendStatus(500);
+    }
+}
+
+export { postOrders, getOrders, getOrderId }
--- src/routers/ordersRouter.js
+++ src/routers/ordersRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { postOrders, getOrders } from "../controllers/ordersController.js";
+import { postOrders, getOrders, getOrderId, getOrderClientId } from "../controllers/ordersController.js";
 import validateOrder from "../middlewares/ordersMiddleware.js";
 
 const router = express.Router();
 
 router.post('/order', validateOrder, postOrders);
 router.get('/orders', getOrders);
+router.get('/orders/:id', getOrderId);
 
 export default router;
